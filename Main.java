196d6080d9752785281bc724d490835154c34918
Pascal <pascal13yt@gmail.com>
2023-11-22 20:51:42 +0100

    V1.0

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         int length;
         int durchlaeufe;
         boolean is = false;
